4eaf2e9581dbbd446671c545d1b0e29820165bdb
Tales Silveira de Souza <tales-silveira@hotmail.com>
2021-08-17 17:19:10 -0300

    Created confirmViewController

---
+++ LaPampa/Controller/Menu/ConfirmChoicesViewController.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class ConfirmChoicesViewController: UIViewController {
+
+    @IBOutlet weak var totalOrderLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
+// MARK: - IBActions
+extension ConfirmChoicesViewController {
+    @IBAction func confirmTouchUpInside(_sender: UIButton) {
+        
+    }
+}
